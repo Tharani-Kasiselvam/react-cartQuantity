c0b3aa4dfada67fe69ec1c4470295e41a76b2e15
Tharani-Kasiselvam <sherley.tharani@gmail.com>
2024-06-30 00:35:17 +0530

    modified styles

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
     - A selected Product is loaded on the webpage
     - based on the Increment and Decrement of the Quantity, the Price of the product changes
     - ContextAPI is used to manage the Product json data and to render the contents
+
+***Author:Tharani K***
--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,16 +1,18 @@
 import { useContext, useState } from "react"
 import { ProductContext } from "../App"
 import '../../src/App.css'
+import { HiPlus } from "react-icons/hi";
+import { HiMinus } from "react-icons/hi";
 
 const CartPage = () => {
     const {product_data} = useContext(ProductContext)
-    const prod_details = product_data[3]
+    const prod_details = product_data[4]
 
     const [cartCount,setCartCount] = useState(1)
 
     const setRating = () => {
       let currentRating = Math.round(prod_details.rating)
-      console.log(currentRating)
+      // console.log(currentRating)
     }
 
     const addCountHandler = () => {
@@ -28,7 +30,7 @@ const CartPage = () => {
 
     const calcTotalPrice = () => {
       let totalPrice = prod_details.price * cartCount
-      console.log(totalPrice)
+      // console.log(totalPrice)
       return totalPrice
     }
 
@@ -36,22 +38,24 @@ const CartPage = () => {
     <div className="main">
         <div className="row" >
           <div className="col-md-6" style={{marginTop:"30px"}}>
-            <img className="card-img-top" src="../prod_1.png" alt="cart_img"/>
+            <img className="card-img-top" src="../prod_1.png" alt="cart_img"
+              style={{height:"250px",width:"200px"}}/>
           </div>
           <div className="col" style={{marginTop:"30px"}}>
             <h3>{prod_details.title}</h3>
             <h6>Description:</h6>
             <p>{prod_details.description}</p>
-            <h6>Brand:</h6>{prod_details.brand}
-            {setRating()}
+            <h6>Brand:</h6><p>{prod_details.brand}</p>
+            <h6>Rating:</h6><p>{prod_details.rating}</p>
+            <p><small><b>Category: </b>{prod_details.category}</small></p>
           </div>
         </div>
         <hr className="line"/>
         <div className="row">
           <div className="col">
-              <button className="rmvCount" onClick = {removeCountHandler}>-</button>
-              <button>{cartCount}</button>
-              <button className="addCount" onClick = {addCountHandler}>+</button>
+              <button className="rmvCount" onClick = {removeCountHandler}><HiMinus /></button>
+              <button className="quantity">{cartCount}</button>
+              <button className="addCount" onClick = {addCountHandler}><HiPlus /></button>
           </div>
           <div className="col" style={{textAlign: "right"}}><b>Price:</b> ₹{prod_details.price}</div>
         </div>
@@ -64,8 +68,9 @@ const CartPage = () => {
         <div className="row">
         <div className="col"><b>TOTAL PRICE:</b></div>
         <div className="col" style={{textAlign: "right"}}><b>₹{calcTotalPrice()}</b></div>
-        <br /> <br />
         </div>
+        <button className="btn btn-primary text-white" style={{width:"180px", margin:"20px"}}>Proceed to Buy</button>
+        <br /> <br />
     </div>
   )
 }
